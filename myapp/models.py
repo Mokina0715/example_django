f5611484cc6caeb8bd147f22242dc9d9807667b6
Mokina0715 <mokina.a15@gmail.com>
2025-07-16 19:10:45 -0500

    Render view

--- myapp/models.py
+++ myapp/models.py
@@ -6,9 +6,14 @@ class CentroComercial(models.Model):
     ciudad = models.CharField(max_length=50)
     direccion = models.CharField(max_length=255)
     telefono = models.CharField(max_length=15, blank=True, null=True)
+    descripcion = models.TextField(blank=True, null=True)
+    url = models.URLField(blank=True, null=True)
 
     def __str__(self):
         return self.nombre
+
+    class Meta:
+        verbose_name_plural = "Centros Comerciales"
 
 class Tienda(models.Model):
     nombre = models.CharField(max_length=100)
@@ -19,3 +24,6 @@ class Tienda(models.Model):
 
     def __str__(self):
         return f"{self.nombre} ({self.categoria}) - {self.centro_comercial.nombre}"
+
+    class Meta:
+        verbose_name_plural = "Locales"
--- myapp/urls.py
+++ myapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
-
+from myapp import views
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('cc_cacique', TemplateView.as_view(template_name='cc_cacique.html'), name='cc_cacique'),
     path('cc_laquinta', TemplateView.as_view(template_name='cc_laquinta.html'), name='cc_laquinta'),
     path('cc_megamall', TemplateView.as_view(template_name='cc_megamall.html'), name='cc_megamall'),
+    path('centros_comerciales', views.lista_centros_comerciales, name='lista_centros_comerciales'),
 ]
